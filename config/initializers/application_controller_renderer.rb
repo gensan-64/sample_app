b441914a4b71623e0f08e7254903ee3858aa338a
gensan-64 <genyuu6240@gmail.com>
2021-08-03 08:32:59 +0000

    7章内容

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -5,4 +5,5 @@
 #     http_host: 'example.org',
 #     https: false
 #   )
-# end
+#
+Refile.secret_key = '16e358400a85a967d28f0c8097442278a38301da6c33c03c604806a7de58112ae355c10121fcf4c4a8e5d5a7ef5c00b2cdd31d015bdc4f63b3912d4af60035fc'
